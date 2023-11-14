0bd24764ae946f564612f996234483198f12ed7b
herman-webdev <herman.web@outlook.com>
2023-11-15 01:45:42 +0300

    jest setup

---
+++ auth/test/jest.e2e.js
@@ -0,0 +1,10 @@
+import sharedConfig from './jest.config';
+
+export default {
+  ...sharedConfig,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
