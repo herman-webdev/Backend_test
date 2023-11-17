027eff2cb046453ec542783505ea05f998511b6f
herman-webdev <herman.web@outlook.com>
2023-11-17 05:03:56 +0300

    validation has been added

--- todo/src/api/dto/createTodo.dto.ts
+++ todo/src/api/dto/createTodo.dto.ts
@@ -1,4 +1,17 @@
-export interface CreateTodoDto {
+import { IsString, MinLength, MaxLength } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsString({ message: 'Title must include only letters or numbers...' })
+  @MinLength(1, { message: 'Password must be at least 1 character...' })
+  @MaxLength(255, {
+    message: 'Title can not be more than 255 characters...',
+  })
   title: string;
+
+  @IsString({ message: 'Title must include only letters or numbers...' })
+  @MinLength(1, { message: 'Password must be at least 1 character...' })
+  @MaxLength(1255, {
+    message: 'Description can not be more than 1255 characters...',
+  })
   description: string;
 }
